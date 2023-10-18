bf003d532a1902af4260b243664b6af3691b6d1b
Rhadijaaaaaa <136619161+Rhadijaaaaaa@users.noreply.github.com>
2023-10-18 08:07:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ document.querySelectorAll('tecla_pom').onclick= tocaSomPom ();
 
 listaDeTeclas[0].onclick =tocaSomPom;
 
-
+let contador=0;
 while(contador<9){
     listaDeTeclas[contador].onclick= TocaSom;
 }
 listadeTeclas[3].onclick -tocaSomPuff;
 
-
+contador= contador + 1;
+.
